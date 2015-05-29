03169078ad7b595a848203335b01dc12c4fd750a
Andrew Chavez <adchavez@gmail.com>
2015-05-28 21:38:24 -0500

    Test for JShint passing, too

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'archieml', 'nodeunit']);
+  grunt.registerTask('test', ['jshint', 'clean', 'archieml', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
